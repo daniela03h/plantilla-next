2192941d4b01bd5cf53da5de8cc9b65ee17eca44
Daniela Londono <daniela0393h@gmail.com>
2024-10-04 08:59:18 -0500

    feat: configuration next intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
 /** @type {import('next').NextConfig} */
+const withNextIntl = createNextIntlPlugin()
 const nextConfig = {
     compiler: {
         styledComponents: true,
     }
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
